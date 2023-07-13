8483ef724850f7878d8f9f38937232dea0923426
JulianMunoz <julian.munoz@applydigital.com>
2023-07-13 17:58:25 -0300

    añadido setState base

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,7 +1,19 @@
 import "./TodoSearch.css";
+import React from "react";
 
 function TodoSearch() {
-  return <input placeholder="Cortar cebolla" />;
+  const [searchValue, setSearchValue] = React.useState(""); // Nomeclatura: state, setState
+
+  console.log("Los usuarios buscan todos de " + searchValue);
+  return (
+    <input
+      placeholder="Cortar cebolla"
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value); // Usar el callback del cambio de estado
+      }}
+    />
+  );
 }
 
 export { TodoSearch };
