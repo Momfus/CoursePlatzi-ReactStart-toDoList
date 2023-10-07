71db83d6f4d0fabff7bc14e48ca2cc698c585594
Julian Muñoz Velázquez <julian.munoz@applydigital.com>
2023-10-06 22:44:30 -0300

    Updates

--- src/TodoList/index.js
+++
@@ -1,8 +0,0 @@
-import "./TodoList.css";
-
-function TodoList({ children }) {
-  // props.children o directametne {children} al destructurar (react automaticamente convierte en children lo que este entre tags)
-  return <ul className="TodoList">{children}</ul>;
-}
-
-export { TodoList };
