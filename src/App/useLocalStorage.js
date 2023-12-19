2c1e34acf05d017382664dc53b9ec15858cec7f7
Julian Muñoz Velázquez <momfus@outlook.com>
2023-12-19 01:52:36 -0300

    Transfer useState to useReducer

--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -2,53 +2,100 @@ import React from "react";
 
 // Por convención, en react, los custom hooks se usa el prefijo "use"
 function useLocalStorage(itemName, initialValue) {
-  const [sincronizedItem, setSincronizedItem] = React.useState(true); // Usado para saber si esta todo sincronizado en las diferentes ventanas del navegador
-  const [item, setItem] = React.useState(initialValue); // Estado interno del esta custom hook (se coloca aca porque puede tener un valor inicial vacio como el indicado anteriormente)
-  const [loading, setLoading] = React.useState(true);
-  const [error, setError] = React.useState(false);
+  const [state, dispatch] = React.useReducer(
+    reducer,
+    initialState({ initialValue })
+  );
 
+  const { sincronizedItem, item, loading, error } = state;
+
+  // Action creators
+  const onError = (error) => dispatch({ type: actionTypes.error, error });
+  const onSuccess = (parsedItem) =>
+    dispatch({ type: actionTypes.success, payload: parsedItem });
+  const onSave = (newItem) =>
+    dispatch({ type: actionTypes.save, payload: newItem });
+  const onSincronize = () => dispatch({ type: actionTypes.sincronize });
+
+  // Main code
   React.useEffect(() => {
     setTimeout(() => {
       try {
         const localStorageItem = localStorage.getItem(itemName);
         let parsedItems;
-
         if (!localStorageItem) {
           localStorage.setItem(itemName, JSON.stringify(initialValue));
           parsedItems = initialValue;
         } else {
           parsedItems = JSON.parse(localStorageItem);
-          setItem(parsedItems);
+          onSuccess(parsedItems);
         }
       } catch (error) {
-        setError(true);
-      } finally {
-        setLoading(false);
-        setSincronizedItem(true);
+        onError(error);
       }
     }, 2000); // Prueba de segundos para simular el tiempo de conexión
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sincronizedItem]); // Lo dejo vacio porque así señalo que no hay dependencias que deba volvers a llamar por cada cambio de estado
 
   const saveItem = (newItem) => {
-    localStorage.setItem(itemName, JSON.stringify(newItem));
-    setItem(newItem);
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      onSave(newItem); // Aquí es donde necesitas llamar a dispatch
+    } catch (error) {
+      onError(error);
+    }
   };
-
   //Sincronizar todo y colocar el loading en true (al cambiar el estado de sincronized, es que se efectua la carga del localstorage)
   const sincronizeItem = () => {
-    setLoading(true);
-    setSincronizedItem(false);
+    onSincronize();
   };
 
   // Asi se exporta para poder usarlo en otro lado (el estado y la función)
-  return {
-    item,
-    saveItem,
-    loading,
-    error,
-    sincronizeItem,
-  };
+  return { item, saveItem, loading, error, sincronizeItem };
 }
+
+const initialState = ({ initialValue }) => ({
+  sincronizedItem: true,
+  loading: true,
+  error: false,
+  item: initialValue,
+});
+
+// se crea un reducer object
+const actionTypes = {
+  error: "ERROR",
+  success: "SUCCESS",
+  save: "SAVE",
+  sincronize: "SINCRONIZE",
+};
+
+const reducerObject = (state, payload) => ({
+  [actionTypes.error]: {
+    ...state,
+    error: true,
+  },
+  [actionTypes.success]: {
+    ...state,
+    error: false,
+    sincronizedItem: true,
+    loading: false,
+    item: payload,
+  },
+  [actionTypes.save]: {
+    ...state,
+    item: payload,
+  },
+  [actionTypes.sincronize]: {
+    ...state,
+    sincronizedItem: false,
+    loading: true,
+  },
+});
+
+const reducer = (state, action) => {
+  // Si no existe el action.type,, devuelve el mismo estado que tenia
+  return reducerObject(state, action.payload)[action.type] || state;
+};
 
 export { useLocalStorage };
